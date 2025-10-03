bdeac554aefb68e885afd85ecb976d29936b7447
duphuoc2k4 <phuocdufavn@gmail.com>
2025-10-03 14:42:45 +0700

    first commit

---
+++ es6/destructuring.js
@@ -0,0 +1,7 @@
+console.log('destructuring object')
+const user = { name: "Bob", age: 25 };
+const { name, age } = user;
+
+// destructuring array
+const numbers = [1, 2, 3];
+const [a, b] = numbers;
